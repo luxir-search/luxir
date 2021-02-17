9d73130811c70c54581f517f1567c3e0662b92ec
yonik <yonik@apache.org>
2021-02-17 12:26:23 -0500

    test multiple segments in reader/writer

--- test/basic_tests/IndexWriterTest.cpp
+++ test/basic_tests/IndexWriterTest.cpp
@@ -19,17 +19,35 @@ public:
 TEST_F(IndexWriterTest, singleSeg) {
   RAMDir dir;
   IndexWriter iw(dir);
-  auto& inverter = iw.getInverter();
-  Inverter::SegFieldPos& segField = inverter.getSegField(field);
+  auto inverter = &iw.getInverter();
+  auto segField = &inverter->getSegField(field);
 
   std::string doc1 = "now is the time for all good men";
-  inverter.startDoc();
-  inverter.index(segField, doc1.data(), doc1.size());
-  inverter.finishDoc();
+  inverter->startDoc();
+  inverter->index(*segField, doc1.data(), doc1.size());
+  inverter->finishDoc();
 
   iw.flush();
 
   IndexReader r1(dir);
   ASSERT_EQ(1, r1.segments().size());
   ASSERT_EQ(1, r1.maxDoc());
+
+  inverter = &iw.getInverter();
+  segField = &inverter->getSegField(field);
+
+  doc1 = "to come to the aid";
+  inverter->startDoc();
+  inverter->index(*segField, doc1.data(), doc1.size());
+  inverter->finishDoc();
+  doc1 = "of their country";
+  inverter->startDoc();
+  inverter->index(*segField, doc1.data(), doc1.size());
+  inverter->finishDoc();
+
+  iw.flush();
+
+  IndexReader r2(dir);
+  ASSERT_EQ(2, r2.segments().size());
+  ASSERT_EQ(3, r2.maxDoc());
 }
