d978b4f4a802d40a51f3d16d152f6057a3b682d4
yonik <yonik@apache.org>
2024-05-04 16:50:02 -0400

    make setting SoluxError in ErrorHolder thread safe

--- src/solux/server/SoluxError.h
+++ src/solux/server/SoluxError.h
@@ -18,46 +18,79 @@ public:
   std::string message;  // guaranteed to be set in the event of an exception, no need to touch eptr just for that.
   std::exception_ptr eptr;
 
+  SoluxError(const std::exception& e) : eptr(std::current_exception()) {
+    message = std::string("Unexpected exception: ").append(e.what());
+    error_code = 1;
+  }
+
   const std::string& what() const {
     return message;
   }
 };
 
-// A generic error holder to propagate errors and return values.  Currently designed to be cheap to copy if no errors.
-// Making a copy if there are errors will normally need dynamic memory allocation.
+// A generic error holder to propagate errors and return values.  Currently designed to be cheap if no errors.
 class ErrorHolder {
+  // different threads may try to set an error state concurrently, so we need some level of concurrency control.
+  std::atomic<SoluxError*> error;
 public:
-  std::unique_ptr<SoluxError> error;
-
   ErrorHolder() = default;
+  ErrorHolder(const ErrorHolder& other) = delete;
+  ErrorHolder& operator=(const ErrorHolder& other) = delete;
 
-  void setException(const std::exception& e) {
-    error = std::make_unique<SoluxError>();
-    error->error_code = 1;
-    error->eptr = std::current_exception();
-    std::string msg = std::string("Unexpected exception: ").append(e.what());
-    // std::stacktrace coming in gcc14 (but not sure if it will provide exception backtrace)
-    // msg += "\n" + boost::stacktrace::to_string(boost::stacktrace::stacktrace());  // not in a released version of boost yet
-    LOG_ERROR(msg);
-    error->message = std::move(msg);
-  }
-
-  ErrorHolder(const ErrorHolder& other) {
-    if (other.error) {
-      error = std::make_unique<SoluxError>(*other.error);
+  ~ErrorHolder() {
+    if (error) {
+      delete error;
     }
   }
 
-  bool ok() {
-    return !error;
+  // Sets a new error only if no error is already set and returns nullptr if successful.
+  // If there was an error previously set, no modification is made and the previous error is returned.
+  SoluxError* setSoluxError(std::unique_ptr<SoluxError>&& e) {
+    SoluxError* err = e.release();
+    SoluxError* expected = nullptr;
+    if (!error.compare_exchange_strong(expected, err)) {
+      // another error is already set.
+      e.reset(err);  // give ptr back to the unique_ptr
+    }
+    return expected;
+  }
+
+  // Sets a new error only if no error is already set and returns nullptr if successful.
+  // If there was an error previously set, no modification is made and the previous error is returned.
+  SoluxError* setException(const std::exception& e) {
+    auto err = std::make_unique<SoluxError>(e);
+    // std::stacktrace coming in gcc14 (but not sure if it will provide exception backtrace)
+    // msg += "\n" + boost::stacktrace::to_string(boost::stacktrace::stacktrace());  // not in a released version of boost yet
+    LOG_ERROR(err->what());
+    return setSoluxError(std::move(err));
+  }
+
+  // Returns true if no error.
+  // Checking if there is an error does not use memory barriers (i.e. you are not guaranteed to
+  // instantly see that an error has occured on another thread.  This should normally be fine as
+  // order between different threads is already undefined w/o additional synchronization.
+  bool ok() const {
+    // We don't really even need an atomic load here since we are just checking for non-zero (i.e. a partial read
+    // wouldn't matter.) but I don't think we can do better w/o an atomic_ref.
+    return error.load(std::memory_order_relaxed) == nullptr;
+  }
+
+  // Returns true if there was an error.
+  bool errored() const {
+    return !ok();
   }
 
   const char* what() {
-    if (error) {
-      return error->what().c_str();
+    SoluxError* err = error.load();
+    if (err) {
+      return err->what().c_str();
     } else {
       return "(ok)";
     }
+  }
+
+  SoluxError* getError() {
+    return error.load();
   }
 };
 
