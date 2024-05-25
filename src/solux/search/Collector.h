7fbf29162353acb985ccb8d733d865fcf8a8c9fa
yonik <yonik@apache.org>
2024-05-24 22:45:06 -0400

    try a new top-k collector that collects 2k, finds median, drops below that

--- src/solux/search/Collector.h
+++ src/solux/search/Collector.h
@@ -32,10 +32,13 @@ public:
 class TopDocsCollector {
   public:
 
+  // Having this packed helped both memory and CPU (presumably better cache hits?)
+  SOLUX_PACKED_START
   struct ScoreDoc {
-   float score;
-   segdoc doc;
-  };
+    float score;
+    segdoc doc;
+  } SOLUX_PACKED_END;
+
 
   int64_t hitCount = 0;
 
@@ -98,5 +101,165 @@ class TopDocsCollector {
 
 };
 
+
+// A different way to get top-k results: Collect results in an array until we have 2k of them.
+// The find the median (kth value), and drop all values less than the median. Repeat collection more results.
+// This has the advantage of not having the bad worst-case behavior of a priority queue and collecting something
+// that is already mostly sorted.  It is also more linear and doesn't degrade as much as k increases.
+// The obvious downside is double the memory usage... we would want a different strategy if someone wants the
+// whole index sorted for example.
+// Also, since minCompetitiveVal is only updated every k docs, this is perhaps more compatible with
+// multi-threaded collection (i.e. check/update a global minCompetitiveVal at the same time you calculate the
+// new minCometitiveVal from the median.)  It also may just be less important to update this minCompetitiveVal
+// in any case.
+//
+// I don't know who created this algorithm, but I learned about it from a blog post by Paul Masurel:
+// https://quickwit.io/blog/top-k-complexity
+//
+// Performance: see the benchmarks in CollectorBM.cpp
+//   For random order, this was slower across the board.  I think perhaps because there are more swaps.
+//   For more complex sorts, we're going to need indirection anyway, so that may change things.
+//   The tricky part of implementing indirection for this would be reusing the slots after you have
+//   dropped half of them.  Seems worse for cache locality.
+//   Update: I tried indirection (see TopScoreCollectorI) and it did improve performance.
+class TopScoreCollector {
+public:
+  using ScoreDoc = TopDocsCollector::ScoreDoc;
+
+  // use the same reversed comparator as the priority-queue based collector so larger values are first
+  // and we can drop values less than the median by simply resizing the vector.
+  constexpr static auto scoreComp = TopDocsCollector::scoreComp;
+
+
+  int64_t hitCount = 0;
+  int64_t k;  // number of top docs we want to find
+  std::vector<ScoreDoc> topDocs;
+  float minCompetitiveVal = std::numeric_limits<float>::lowest();
+
+  TopScoreCollector(int64_t topCount) : k(topCount) {
+    assert(topCount > 0);
+    // topDocs.reserve(topCount*2);  // reserve or not?
+  }
+
+  void dropHalf() {
+    // iterator at the kth element is at index k-1.
+    std::nth_element(topDocs.begin(), topDocs.begin()+k-1, topDocs.end(), scoreComp);
+    minCompetitiveVal = topDocs[k-1].score;  // kth value is at index k-1
+    topDocs.resize(k);
+  }
+
+  void collect(int32_t segment, int32_t docid, float score) {
+    hitCount++;
+    if (score > minCompetitiveVal) {
+      topDocs.push_back({score, segdoc(segment, docid)});
+      if (topDocs.size() >= (k<<1)) {
+        dropHalf();
+      }
+    }
+  }
+
+  int64_t totalHits() const {
+    return hitCount;
+  }
+
+  int64_t size() const {
+    return topDocs.size();
+  }
+
+  void sort() {
+    if (topDocs.size() > k) {
+      dropHalf();
+    }
+    std::sort(topDocs.begin(), topDocs.end(), scoreComp);
+  }
+
+  // merge other into this.
+  void merge(TopDocsCollector& other) {
+    auto newHitCount = hitCount + other.hitCount;
+    // The generic way...  Could probably be optimized.
+    // 1) if other.minCompetitiveVal is greater than our minCompetitiveVal, we can just append all of other's docs.
+    // 2) we could skip the comparisons anyway if there is space?
+    for (auto& sd: other.topDocs) {
+      collect(sd.doc.segment(), sd.doc.docId(), sd.score);
+    }
+    hitCount = newHitCount;
+  }
+
+
+};
+
+class TopScoreCollectorI {  // I for "indirect"
+public:
+  using index_t = int16_t;
+  using ScoreDoc = TopDocsCollector::ScoreDoc;
+
+  int64_t hitCount = 0;
+  int64_t k;  // number of top docs we want to find
+  std::vector<ScoreDoc> topDocs;
+  std::vector<index_t> indexes;
+  int64_t held = 0;  // number of valid indexes currently held.  We don't downside indexes.
+  float minCompetitiveVal = std::numeric_limits<float>::lowest();
+
+
+  TopScoreCollectorI(int64_t topCount) : k(topCount) {
+    assert(topCount > 0);
+    // assign slot indexes to start off with, just to see what the performance would be.
+    // downside is that this uses max memory to start, even if no hits.
+    topDocs.reserve(topCount*2);
+    indexes.reserve(topCount*2);
+    for (int i = 0; i < topCount*2; i++) {
+      indexes.push_back(i);
+    }
+  }
+
+  void dropHalf() {
+    auto* arr = &topDocs[0];  // does this help not direct through this for every comparison?
+    auto scoreCompI = [arr](index_t a, const index_t b) {
+      return arr[a].score > arr[b].score;
+    };
+    // iterator at the kth element is at index k-1.
+    std::nth_element(indexes.begin(), indexes.begin()+k-1, indexes.end(), scoreCompI);
+    minCompetitiveVal = arr[indexes[k-1]].score;  // kth value is at index k-1
+    held = k;  // drops half
+  }
+
+  void collect(int32_t segment, int32_t docid, float score) {
+    hitCount++;
+    if (score > minCompetitiveVal) {
+      topDocs[indexes[held++]] = {score, segdoc(segment, docid)};
+      if (held >= (k<<1)) {
+        dropHalf();
+      }
+    }
+  }
+
+  int64_t totalHits() const {
+    return hitCount;
+  }
+
+  int64_t size() const {
+    return held;
+  }
+
+  void sort() {
+    if (held > k) {
+      dropHalf();
+    }
+    auto* arr = &topDocs[0];  // does this help not direct through this for every comparison?
+    auto scoreCompI = [arr](index_t a, const index_t b) {
+      return arr[a].score > arr[b].score;
+    };
+    std::sort(indexes.begin(), indexes.end(), scoreCompI);
+  }
+
+  // merge other into this.
+  void merge(TopDocsCollector& other) {
+    /*
+     * not implemented yet.
+     */
+  }
+
+
+};
 
 } // namespace solux
---
+++ test/bench/CollectorBM.cpp
@@ -0,0 +1,43 @@
+#include <charconv>
+#include <test/SegmentTest.h>
+#include "bench/solux_bench.h"
+#include "solux/index/Inverter.h"
+
+using namespace solux;
+
+template<typename T>
+static void BM_Collector(benchmark::State& state, int32_t nDocs, bool sorted) {
+  // Rng rng(SoluxTest::rng_seed);
+  Rng rng(1);
+   uint32_t topK = state.range(0); // number of iterators / streams to merge
+
+  double ret = 0;
+  for (auto _ : state) {
+    T collector(topK);
+    for (int i = 0; i < nDocs; i++) {
+      float score = sorted ? float(i) : float(rng());
+      collector.collect(0, i, score);
+    }
+    collector.sort();
+    ret = collector.topDocs[0].score;
+    benchmark::DoNotOptimize(ret);
+  }
+  state.counters["topscore"] = ret;  // sanity check that different implementations are agreeing.
+}
+
+static void BM_CollectorPQ(benchmark::State& state, int32_t nDocs, bool sorted) {
+  BM_Collector<TopDocsCollector>(state, nDocs, sorted);
+}
+static void BM_CollectorMed(benchmark::State& state, int32_t nDocs, bool sorted) {
+  BM_Collector<TopScoreCollector>(state, nDocs, sorted);
+}
+static void BM_CollectorMedI(benchmark::State& state, int32_t nDocs, bool sorted) {
+  BM_Collector<TopScoreCollectorI>(state, nDocs, sorted);
+}
+
+constexpr int32_t ndocs = 1000000;
+BENCHMARK_CAPTURE(BM_CollectorPQ, rand, ndocs, false)->Range(1, 1<<10);
+BENCHMARK_CAPTURE(BM_CollectorMed, rand, ndocs, false)->Range(1, 1<<10);
+BENCHMARK_CAPTURE(BM_CollectorMedI, rand, ndocs, false)->Range(1, 1<<10);
+BENCHMARK_CAPTURE(BM_CollectorPQ, sorted, ndocs, true)->Range(1, 1<<10);
+BENCHMARK_CAPTURE(BM_CollectorMed, sorted, ndocs, true)->Range(1, 1<<10);
