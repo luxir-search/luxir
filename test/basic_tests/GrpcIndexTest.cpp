505992f90db503708781d10a6d1f24f06a553918
yonik <yonik@apache.org>
2025-05-24 13:36:13 -0400

    add very basic facet request to grpc thread safe search test

--- test/basic_tests/GrpcIndexTest.cpp
+++ test/basic_tests/GrpcIndexTest.cpp
@@ -640,6 +640,7 @@ TEST_F(GrpcIndexTest, threadsafeIndex) {
   int64_t nDocs = 100;  // pump this up for good stress testing.
   int streamingPercent = 50;  // percent of the requests that use streaming
   int commitPercent = 10;
+  uint32_t percentFacet = 50; // percent of the requests that use facets
 
   // clear the index
   solux::test::CollectionHelper ch("main");
@@ -655,11 +656,17 @@ TEST_F(GrpcIndexTest, threadsafeIndex) {
     for (auto& field : retrieveFields) {
       topDocs.mutable_fields()->Add(field);
     }
-
     auto& topQuery = *topDocs.mutable_query()->mutable_match();
     topQuery.set_field("id");
     topQuery.mutable_val()->set_s(std::to_string(docid));
     req.set_request_id(std::to_string(docid));  // set request id to the id so we know what doc we are looking for
+
+    // Sometimes add integer facet request for id_i
+    if (SplitMix64(docid)() % 100 < percentFacet) {
+      auto& facet = *(*req.mutable_ops())["f"].mutable_field_facet();
+      facet.set_field("id_i");
+      facet.set_limit(10);
+    }
   };
 
   ResponseChecker responseChecker = [&](int64_t docid, const solux::proto::SearchResponse& response) {
@@ -667,6 +674,16 @@ TEST_F(GrpcIndexTest, threadsafeIndex) {
     ASSERT_EQ(1, docList.matches());  // FIXME!  this comes up as "2" now sometimes with nDocs=100????
     ASSERT_EQ(docList.columns_size(), retrieveFields.size()); // this might change in the future.
     verifyDoc(docid, docList.columns());
+
+    // do same calculation to see if facet was requested
+    if (SplitMix64(docid)() % 100 < percentFacet) {
+      // verify the facet response.  It should be a single bucket with id_i=docid and count=1
+      auto& facetResult = response.ops().at("f").facet();
+      ASSERT_EQ(1, facetResult.bucket_ids().col_i().v().size());
+      ASSERT_EQ(docid, facetResult.bucket_ids().col_i().v()[0]);
+      ASSERT_EQ(1, facetResult.counts().size());
+      ASSERT_EQ(1, facetResult.counts()[0]);
+    }
   };
 
   doThreadSafeSearch(nThreads, nDocs, nDocs, requestCreator, responseChecker);
