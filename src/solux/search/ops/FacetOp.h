29b1d17fb45415534d857fd86782dba2e5b23bfe
jason <jseeley64@gmail.com>
2026-05-28 15:50:53 -0400

    fix: enable facet sub-op sorting (inverted condition in FacetReq::init)

--- src/solux/search/ops/FacetOp.h
+++ src/solux/search/ops/FacetOp.h
@@ -45,7 +45,8 @@ public:
 
   void init() override {
     SearchOp::init();
-    if (sorts.empty()) {
+    // TODO: add a test for faceting sorted by an inline sub-op (this branch); currently uncovered.
+    if (!sorts.empty()) {
       for (auto& sort : sorts) {
         auto iter = subOps.find(sort.field());
         if (iter != subOps.end()) {
